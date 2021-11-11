ef7fca290de4aec7ea39e9340cc05149312311fe
MridulSheikh <prince9nazir@gmail.com>
2021-11-11 21:19:38 +0600

    new changes

--- src/components/procced to shipping/Shipping.js
+++ src/components/procced to shipping/Shipping.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const Shipping = () => {
     const [product, setProduct] = useState([])
+    const {user} = useAuth()
     const {ProductId} = useParams()
     useEffect(()=>{
         fetch(`http://localhost:5000/product/${ProductId}`)
@@ -13,7 +15,7 @@ const Shipping = () => {
         .then(data => setProduct(data))
     },[])
      //form
-     const { register, handleSubmit, reset} = useForm();
+     const { register, handleSubmit, reset, onblur} = useForm();
      const onSubmit = data =>{
         axios.post("http://localhost:5000/Orders",data)
         .then(res =>{
@@ -32,18 +34,17 @@ const Shipping = () => {
         <Card.Body>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>
-       your brand id : {product._id}
       </Card.Text>
     </Card.Body>
   </Card>
         </Col>
         <Col sm={1} md={4} style={{backgroundColor:"#fff"}} className="mt-5 pt-md-3">
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("brand_id")} placeholder="please copy past brand Id" className='d-block w-100 mt-3 fs-5 p-2' required  />
-            <input  style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("Name", { required: true, maxLength: 60 })} placeholder="name" className='d-block w-100 mt-3 fs-5 p-2' required  />
-            <input style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("email", { required: true, maxLength: 60 })} placeholder="your email" className='d-block w-100 mt-3 fs-5 p-2' required  />
-            <input  style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}}{...register("phone", { required: true})} placeholder="phone number" className='d-block w-100 mt-3 fs-5 p-2' required  />
-            <input  style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("address")} placeholder="address" className='d-block w-100 mt-3 fs-5 p-2' required  />
+            <input  onBlur={onblur} defaultValue={product._id} style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("brand_id")} placeholder="please copy past brand Id" className='d-block w-100 mt-3 fs-5 p-2' required  />
+            <input onBlur={onblur} defaultValue={user.displayName} style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("Name", { required: true, maxLength: 60 })} placeholder="name" className='d-block w-100 mt-3 fs-5 p-2' required  />
+            <input onBlur={onblur} defaultValue={user.email}  style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("email", { required: true, maxLength: 60 })} placeholder="your email" className='d-block w-100 mt-3 fs-5 p-2' required  />
+            <input onBlur={onblur}   style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}}{...register("phone", { required: true})} placeholder="phone number" className='d-block w-100 mt-3 fs-5 p-2' required  />
+            <input onBlur={onblur}   style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} {...register("address")} placeholder="address" className='d-block w-100 mt-3 fs-5 p-2' required  />
             <input  style={{outline:"none", borderBottom:"2px solid #D80178", borderTop:"0px", borderLeft:"0px",borderRight:"0px"}} type="submit"  className='d-block w-100 mt-3 py-1 border-0  rounded text-light' style={{background: "linear-gradient(329deg, rgba(71,0,93,0.8547794117647058) 0%, rgba(147,0,147,1) 33%)"}} />
           </form>
         </Col>
